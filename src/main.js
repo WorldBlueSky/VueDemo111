176494ab38a671adcb1b3808d04771506230170b
WorldBlueSky <ChenRuiQi21@163.com>
2022-09-24 01:31:40 +0800

    更新

--- src/main.js
+++ src/main.js
@@ -6,7 +6,9 @@ import 'element-ui/lib/theme-chalk/index.css';
 import Element from 'element-ui';
 import App from './App'
 import router from './router'
+import axios from 'axios'
 
+Vue.prototype.$axios = axios
 Vue.use(ElementUI);
 Vue.use(Element, { size: 'small', zIndex: 3000 });
 Vue.config.productionTip = false
